d03d0a4d1f9d75421c7e7a981d506ed5c3f141ba
Fred Z <freezed@users.noreply.github.com>
2017-12-26 01:22:14 +0100

    Apprendre Python - UPDATE 1.4-tp-bisextile.py: bonus perso hors énoncé

--- programmer_en_python/1.4-tp-bisextile.py
+++ programmer_en_python/1.4-tp-bisextile.py
@@ -16,3 +16,13 @@ elif (annee % 4) == 0 and (annee % 100) != 0:
 else:
     print(annee," n'est PAS bisextile.")
 liste = []
+
+# La suite ne fait pas parti du TP: bonus perso
+
+print("Voici la liste des annees bisextiles entre 1900 & 2030 :")
+
+for a in range(1900,2030):
+    if ((a % 400) == 0) or ((a % 4) == 0 and (a % 100) != 0):
+        liste.append(a)
+
+print(liste)
