ad4fac76eee04fa7635edd665b7d4d7ec14fa15d
Fred Z <freezed@users.noreply.github.com>
2017-12-27 00:49:57 +0100

    Apprendre Python - UPDATE 1.5-continue: essai avec boucle for. FAIL

--- programmer_en_python/TODO/1.5-continue.py
+++ programmer_en_python/TODO/1.5-continue.py
@@ -6,11 +6,23 @@
 # OpenClassrooms - Apprenez à programmer en Python - Les boucles
 # https://openclassroom
 
+print("Fonctonnement a reproduire:")
 i = 1
-while i < 20: # Tant que i est inférieure à 20
+while i < 20:
     if i % 3 == 0:
-        i += 4 # On ajoute 4 à i
-        print("On incrémente i de 4. i est maintenant égale à", i)
-        continue # On retourne au while sans exécuter les autres lignes
+        i += 4
+        print("On incremente i de 4. i =", i)
+        continue
     print("La variable i =", i)
     i += 1 # Dans le cas classique on ajoute juste 1 à i
+i = 0
+
+print("boucle «for», probleme de scope avec la valeur de i")
+for i in range(1,20):
+    if i % 3 == 0:
+        i += 4
+        print("On incremente i de 4. i = ", i)
+    else:
+        print("La variable i =", i)
+        i += 1
+
