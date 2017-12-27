3feb056858e9fd75589fd2d697445c2052febc4b
Fred Z <freezed@users.noreply.github.com>
2017-12-28 00:32:16 +0100

    Apprendre Python - ADD 1.9-zCasino.py: ennoncé du TP

---
+++ programmer_en_python/1.9-zCasino.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 1.9-zCasino.py : petit jeu de roulette très simplifié
+
+# OpenClassrooms - Apprenez à programmer en Python - TP : tous au ZCasino
+# https://openclassrooms.com/courses/apprenez-a-programmer-en-python/tp-tous-au-zcasino
+
+#Le joueur mise sur un numéro compris entre 0 et 49
+#En choisissant son numéro, il mise
+#Les numéros pairs sont de couleur noire, les numéros impairs sont de couleur rouge
+#Quand la roulette s'arrête:
+#	Si le numéro gagnant est celui du joueur: gain = mise + (3x la mise)
+#	Si le numéro gagnant et celui misé sont de la même couleur: gain =
+mise + (50% de la mise)
+#	Sinon la mise est perdue
