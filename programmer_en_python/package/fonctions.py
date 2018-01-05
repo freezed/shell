bb955a5fbb5f7dc616b88c8f0e700bae604a2e9a
Fred Z <freezed@users.noreply.github.com>
2018-01-05 02:06:36 +0100

    Ajout de la fonct° «afficher_flottant»|UPDATE fonctions.py|Apprendre Python

--- programmer_en_python/package/fonctions.py
+++ programmer_en_python/package/fonctions.py
@@ -6,10 +6,12 @@
 # OpenClassrooms - Apprenez à programmer en Python - Modularité
 # https://openclassroom/courses/apprenez-a-programmer-en-python/pas-a-pas-vers-la-modularite-2-2
 
-"""Module de test, contient une fonction _table_"""
+"""Module de test, contient les fonctions crees durant le cours
+OpenClassrooms - Apprenez à programmer en Python"""
 
 def table(n, m=11):
-    """Affiche la table de multiplication d'un nombre
+    """Chapitre 1.7 - Modularite
+    Affiche la table de multiplication d'un nombre
     de 1 * n
     a m * n """
     for i in range(1, m):
@@ -18,3 +20,21 @@ def table(n, m=11):
 # test de _table_
 if __name__ == "__main__":
     table(3)
+
+def afficher_flottant(flot):
+
+    """Chapitre 2.3 - Entre chaînes et listes
+    Affiche un nombre a virgule flottante tronque a 3 decimales
+    et remplace le point (.) par une virgule (,)"""
+
+    string = str(flot)
+    splited_string = string.split('.')
+    entier = splited_string[0]
+
+    if len(splited_string[1]) <= 3:
+        decimal = splited_string[1][:len(splited_string[1])]
+    else:
+        decimal = splited_string[1][:3]
+
+    flotant_formate =  ','.join((entier,decimal))
+    print(flotant_formate)
